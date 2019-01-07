dc1d3b8945bbbf8a562bc5d2e093c8bf6e45a6e3
Bryan Jeanty <bryan.jeanty@outlook.com>
2019-01-07 17:19:44 -0500

    Added title and nav bar to home page

--- app.rb
+++ app.rb
@@ -2,6 +2,26 @@ require "sinatra"
 
 get "/" do
     "
+    <head>
+        <title>Pamela's Bakery</title>
+    <head>
+    <body>
+        <header>
+            <h1>Pamela's Bakery</h1>
+            <nav>
+                <li><a>Home</a></li>
+                <li><a>Shop</a></li>
+                <li><a>About</a></li>
+                <li><a>Contact</a></li>
+            </nav>
+        </header>
+
+        <main>
+        </main>
+
+        <footer>
+        </footer>
+    </body>
     "
 end
 
