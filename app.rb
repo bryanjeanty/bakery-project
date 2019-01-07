febda966c7f4194b338b307071931c04da90b252
Bryan Jeanty <bryan.jeanty@outlook.com>
2019-01-07 17:42:05 -0500

    Added stylesheet and script file

--- app.rb
+++ app.rb
@@ -4,15 +4,17 @@ get "/" do
     "
     <head>
         <title>Pamela's Bakery</title>
-    <head>
+        <link rel='stylesheet' type='text/css' href='style.css'/>
+    </head>
     <body>
         <header>
+            <div class='top-banner'></div>
             <h1>Pamela's Bakery</h1>
             <nav>
-                <li><a>Home</a></li>
-                <li><a>Shop</a></li>
-                <li><a>About</a></li>
-                <li><a>Contact</a></li>
+                <li><a href='#'>Home</a></li>
+                <li><a href='#'>Shop</a></li>
+                <li><a href='#'>About</a></li>
+                <li><a href='#'>Contact</a></li>
             </nav>
         </header>
 
@@ -22,6 +24,7 @@ get "/" do
         <footer>
         </footer>
     </body>
+    <script src='script.js'></script>
     "
 end
 
