dfeb3ea2d805b830149d21d99086fd53e4f4521e
Bryan Jeanty <bryan.jeanty@outlook.com>
2019-01-11 18:34:04 -0500

    Removed HTML from controller

--- app.rb
+++ app.rb
@@ -1,44 +1,5 @@
 require "sinatra"
 
-get "/home" do
-    "
-    <head>
-        <title>Friendly Bakery</title>
-        <link rel='stylesheet' type='text/css' href='style.css'/>
-    </head>
-    <body>
-        <header>
-            <div class='top-banner'></div>
-            <h1>Friendly Bakery</h1>
-            <nav>
-                <li><a href='#'>Home</a></li>
-                <li><a href='#'>Shop</a></li>
-                <li><a href='#'>About</a></li>
-                <li><a href='#'>Contact</a></li>
-            </nav>
-        </header>
-
-        <main>
-        </main>
-
-        <footer>
-        </footer>
-    </body>
-    <script src='script.js'></script>
-    "
-end
-
-get "/shop" do
-    "
-    "
-end
-
-get "/about" do
-    "
-    "
-end
-
-get "/contact" do
-    "
-    "
+get "/" do
+    erb(:index)
 end
